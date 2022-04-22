361efa04d4c68568978b68bad01c7b3055935839
Muhammad Riyead Rahman <helloworld.asset@gmail.com>
2022-04-22 23:01:55 +0600

    useCallback-useMemo_Hook lesson complete

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,20 +1,39 @@
-import MyDateCount from './Components/MyDateCount';
-import { useState } from 'react';
-
+import { useCallback, useMemo, useState } from "react";
+import Button from "./Components/Button";
+import ShowCount from "./Components/ShowCount";
+import Title from "./Components/Title";
 
 function App() {
-  const [show, setShow] = useState(true);
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
+
+  const incrementByOne = useCallback(() => {
+    setCount1((prevCount1) => prevCount1 + 1);
+  }, []);
+
+  const incrementByFive = useCallback(() => {
+    setCount2((prevCount2) => prevCount2 + 5);
+  }, []);
+
+  const isEvenOrOdd = useMemo(() => {
+    let i = 0;
+    while (i < 1000000000) i += 1; // costly operation
+    return count1 % 2 === 0;
+  }, [count1]);
+
   return (
-    <div className="App">
-      <div>
-          {show && <MyDateCount />}
-      </div>
-      <p>
-        <button type='button' onClick={() => setShow((prevShow) => !prevShow)}>
-          {show ? 'Hide post' : 'Show post'}
-        </button>
-      </p>
-      
+    <div className='App'>
+      <Title />
+      <ShowCount count={count1} title='Counter 1' />
+      <span>{isEvenOrOdd ? "Even" : "Odd"}</span>
+      <Button type='button' handleClick={incrementByOne}>
+        Increment by one
+      </Button>
+      <hr />
+      <ShowCount count={count2} title='counter 2' />
+      <Button type='button' handleClick={incrementByFive}>
+        Increment by Five
+      </Button>
     </div>
   );
 }
