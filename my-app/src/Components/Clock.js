250a3ff6427baedf5aa728678aaa547b5558ce21
Your Name <helloworld.asset@gmail.com>
2022-10-23 11:56:37 +0600

    ntd

--- my-app/src/Components/Clock.js
+++ my-app/src/Components/Clock.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Button from './Button';
 
 class Clock extends React.Component {
-    state = { date: new Date(), locale: 'en-US' };
+    state = { date: new Date(), locale: 'bn-BD' };
 
     componentDidMount() {
         this.clockTimer = setInterval(() => this.tick(), 1000);
@@ -25,21 +25,23 @@ class Clock extends React.Component {
     }
 
     render() {
-        console.log('clock component rendered');
         const { date, locale } = this.state;
         return (
             <div>
                 <h1 className="heading">
                     <span className="text">{date.toLocaleTimeString(locale)}</span>
                 </h1>
-                <Button change={this.handleClick} locale="en-US">
-                    Click here
-                </Button>
+                {locale === 'bn-BD' ? (
+                    <Button change={this.handleClick} locale="en-US" show={false} enable={false} />
+                ) : (
+                    <Button change={this.handleClick} locale="bn-BD" show enable />
+                )}
             </div>
         );
     }
 }
 
 export default Clock;
+
 
 
